55b84dd8120f32d3b3a52cd517005d1ca3444602
Tanvir Hossain <tanvir@disystem.net>
2024-10-23 08:41:24 +0600

    Navber and Footer page add

--- src/configs/Layout.js
+++ src/configs/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 
+import Navber from '../views/partial/Navber';
+import Footer from '../views/partial/Footer';
+
 
 const Layout = () => {
     return (
         <>
+            <Navber />
             <Outlet />
+            <Footer />
         </>
     );
 };
---
+++ src/views/partial/Navber.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navber = () => {
+  return (
+    <>
+      <h1>Navber</h1>
+    </>
+  )
+}
+
+export default Navber
